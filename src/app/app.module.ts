0423544437b695afa847f1af7a46c011973dc97a
Rogelio Orona <Rogelio_Orona@epam.com>
2018-03-01 16:50:14 -0600

    Add firebase support and custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,31 @@ import { CountClicksDirective } from './directives/count-clicks.directives';
 import { DetailComponent } from './detail/detail.component';
 import { PlacesComponent } from './places/places.component';
 import { ContactComponent } from './contact/contact.component';
+import { PlacesService } from './services/places.service';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { CreateComponent } from './create/create.component';
+import { HttpClientModule } from "@angular/common/http";
+import { LinkifystrPipe } from './pipes/linkifystr.pipe';
 
 const appRoutes: Routes = [
   {path: '', component: PlacesComponent},
   {path: 'places', component: PlacesComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'create/:id', component: CreateComponent},
   {path: 'detail/:id', component: DetailComponent}
 ]
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBHAiZS4gVXJtFzjsKk2Ucvkh3010VBd14",
+    authDomain: "businessmap-1519793269506.firebaseapp.com",
+    databaseURL: "https://businessmap-1519793269506.firebaseio.com",
+    storageBucket: "businessmap-1519793269506.appspot.com",
+    messagingSenderId: "581169443040"
+};
+
 
 @NgModule({
   declarations: [
@@ -26,7 +44,9 @@ const appRoutes: Routes = [
     HighlightDirective,
     CountClicksDirective,
     DetailComponent,
-    ContactComponent
+    ContactComponent,
+    CreateComponent,
+    LinkifystrPipe
   ],
   imports: [
     BrowserModule,
@@ -34,9 +54,13 @@ const appRoutes: Routes = [
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCT88f5iaFmgYqKcvs7HpJfV3CeYzu1aKU'
     }),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PlacesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlacesService } from '../services/places.service';
 
 @Component({
   selector: 'app-places',
@@ -8,19 +9,12 @@ import { Component } from '@angular/core';
 export class PlacesComponent {
   title = 'BusinessMap';
 
-  places: any = [
-    { id: 1, plan: 'paid', near: 1, distance: 1, active: true, name: 'Flower Shop' },
-    { id: 2, plan: 'free', near: 1, distance: 2, active: true, name: 'Donuts' },
-    { id: 3, plan: 'free', near: 2, distance: 7, active: true, name: 'PetStore' },
-    { id: 4, plan: 'paid', near: 3, distance: 15, active: false, name: 'Barbery' },
-    { id: 5, plan: 'free', near: 3, distance: 20, active: false, name: 'Cookies' },
-    { id: 6, plan: 'free', near: 3, distance: 27, active: true, name: 'Sushi Roll' }
-  ];
+  places = null;
 
-  lat: number = 20.6017929;
-  lng: number = -103.4109716;
-
-  constructor() {
-
+  constructor(private placesServices: PlacesService) {
+    placesServices.getPlaces()
+      .valueChanges().subscribe((places) => {
+        this.places = places;
+      });
   }
 }
