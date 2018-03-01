9cacec71c11db7cea423087aac1c42f741499359
Rogelio Orona <Rogelio_Orona@epam.com>
2018-02-28 19:32:18 -0600

    Add contact, detail and place components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BusinessMap';
-
-  places:any = [
-    {plan:'paid', near:1, distance:1, active:true, name: 'Flower Shop'},
-    {plan:'free', near:1, distance:2, active:true, name: 'Donuts'},
-    {plan:'free', near:2, distance:7, active:true, name: 'PetStore'},
-    {plan:'paid', near:3, distance:15, active:false, name: 'Barbery'},
-    {plan:'free', near:3, distance:20, active:false, name: 'Cookies'},
-    {plan:'free', near:3, distance:27, active:true, name: 'Sushi Roll'}
-  ];
-
-  lat:number = 20.6017929;
-  lng:number = -103.4109716;
-
-  constructor(){
-
-  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './directives/highlight.directive';
 import { CountClicksDirective } from './directives/count-clicks.directives';
+import { DetailComponent } from './detail/detail.component';
+import { PlacesComponent } from './places/places.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  {path: '', component: PlacesComponent},
+  {path: 'places', component: PlacesComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'detail/:id', component: DetailComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    PlacesComponent,
     HighlightDirective,
-    CountClicksDirective    
+    CountClicksDirective,
+    DetailComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCT88f5iaFmgYqKcvs7HpJfV3CeYzu1aKU'
-    })
+    }),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
